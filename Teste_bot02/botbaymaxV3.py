1f01a5f0dd70560f42f10449148c89bc7f63aca8
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-03-29 23:40:13 -0300

    Updating

--- Teste_bot02/botbaymaxV3.py
+++ Teste_bot02/botbaymaxV3.py
@@ -1,44 +1,77 @@
 import logging
-import sqlite3
-
-from sqlite3 import Error
+import requests
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          RegexHandler, ConversationHandler, CallbackQueryHandler)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-###########################################################################################
+
 ###########################################################################################
 ###################################### BOT ################################################
 ###########################################################################################
-###########################################################################################    
 
 ########## Entrada do usuario
 
-def entradaInst(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+#def entradaInst(update: Update, context: CallbackContext) -> None:
+#    update.message.reply_text("Informe o número da instalação! \n\nEx.: /instalacao 1234567")
 
-def entradaMed(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+#def entradaMed(update: Update, context: CallbackContext) -> None:
+#    update.message.reply_text("Informe o número do equipamento de medição! \n\nEx.: /medidor 7012345678")
 
-def entradaContrato(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+STATE1 = 1
+STATE2 = 2
 
-def entradaContato(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
 
-def entradaViz(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def entradaContrato(update, context):
+    try:
+        username = update.message.from_user.username
+        firstName = update.message.from_user.first_name
+        lastName = update.message.from_user.last_name
+        message = "Olá, "+ firstName + " " + lastName + "!" + " \n\nInforme o número da conta contrato."
+        update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
+        return STATE1
+    except Exception as e:
+        print(str(e))
 
-def entradaKML(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
 
-def entradaImg(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def inputContrato(update, context):
+    contacontrato = update.message.text
+    print(contacontrato)
+    if len(contacontrato) <= 10:
+        message = """Número da conta contrato está incorreto... 
+                        \nPor gentileza informe o número correto!"""
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE1
+    else:
+        message = "Segue conforme solicitado: " +  "Ajudo em algo mais?"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return ConversationHandler.END
+
+def inputContrato2(update, context):
+    contacontrato = update.message.text
+    message = "Segue conforme solicitado: " + "Ajudo em algo mais?"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    return ConversationHandler.END
+
+def cancel(update, context):
+    return ConversationHandler.END
+
+
+#def entradaContato(update: Update, context: CallbackContext) -> None:
+#    update.message.reply_text("Informe o número da instalação! \n\nEx.: /instalacao 1234567")
+
+#def entradaViz(update: Update, context: CallbackContext) -> None:
+#    update.message.reply_text("Informe o número da instalação! \n\nEx.: /instalacao 1234567")
+
+#def entradaKML(update: Update, context: CallbackContext) -> None:
+#    update.message.reply_text("Informe o número da MRU! \n\nEx.: /mru 01128101")
+
+#def entradaImg(update: Update, context: CallbackContext) -> None:
+#    update.message.reply_text(update.message.text)
   
 
 ###########################################################################################
-###########################################################################################
 ################################ BOT ######################################################
-###########################################################################################
 ###########################################################################################
 
 ########## Verifica se tem mensagem nova
@@ -51,57 +84,9 @@ logger = logging.getLogger(__name__)
 
 ########## iniciando o bot
 
-def start(update: Update, context: CallbackContext) -> None:
+def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
-
-###########################################################################################
-###########################################################################################
-################################## BANCO DE DADOS #########################################
-###########################################################################################
-###########################################################################################
-
-########## Criar Conexão com BD e a tabela
-
-########## Realizar Consultas - Por Conta Contrato
-
-def ConexaoBanco():
-    caminho = "C:\\Projetos_JEB\\BD_Leitura\\BD_Leitura.db" # caminho do banco - "C:\\Projetos_JEB\\BD_Leitura\\BD_Leitura.db"
-    con = None
-
-    try:
-        con=sqlite3.connect(caminho)
-    except Error as ex:
-        print(ex)
-    return con
-
-vcon=ConexaoBanco()   
-
-def consultaContrato(conexao,sql):
-    c=conexao.cursor()
-    c.execute(sql)
-    resultado=c.fetchall()
-    return resultado
-
-entradaCont = entradaContrato
-
-def resultadoCont():
-    vsql= f"SELECT * FROM BD_Leitura WHERE contrato = {entradaCont}" #interpolação para entrada do usuario
-    res=consultaContrato(vcon,vsql)   
-    if res:
-        for r in [res]:
-            for retorno  in r:
-                resultado = {retorno}
-                print(resultado)
-    else:
-        print(f"\nNúmero invalido ou não localizado!\n")
-
-vcon.close()
-
-###########################################################################################################   
-###########################################################################################################   
-###########################################################################################################   
+    update.message.reply_text('Hi!')  
 
 ########## BOT 
 
@@ -111,21 +96,32 @@ def main() -> None:
     updater = Updater("1757548140:AAGqdAGKSoIDJLCIB_NxuQM1TSpKO1RkG94")
 
     # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+    #dispatcher = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("instalacao", entradaInst))
-    dispatcher.add_handler(CommandHandler("medidor", entradaMed))
-    dispatcher.add_handler(CommandHandler("contrato", resultadoCont))
-    dispatcher.add_handler(CommandHandler("contato", entradaContato))
-    dispatcher.add_handler(CommandHandler("vizinhos", entradaViz))
-    dispatcher.add_handler(CommandHandler("kml", entradaKML))
-    dispatcher.add_handler(CommandHandler("imagem", entradaImg))
+    #dispatcher.add_handler(CommandHandler("instalacao", entradaInst))
+    #dispatcher.add_handler(CommandHandler("medidor", entradaMed))
+    #dispatcher.add_handler(CommandHandler("contrato", entradaContrato))
+    #dispatcher.add_handler(CommandHandler("contato", entradaContato))
+    #dispatcher.add_handler(CommandHandler("vizinhos", entradaViz))
+    #dispatcher.add_handler(CommandHandler("kml", entradaKML))
+    #dispatcher.add_handler(CommandHandler("imagem", entradaImg))
+
+    #updater.dispatcher.add_handler(CommandHandler('start', welcome))
+
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler('contrato', entradaContrato)],
+        states={
+            STATE1: [MessageHandler(Filters.text, inputContrato)],
+            STATE2: [MessageHandler(Filters.text, inputContrato2)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)])
+    updater.dispatcher.add_handler(conversation_handler)    
 
     # on noncommand i.e message - echo the message on Telegram
     #dispatcher.add_handler(MessageHandler(consultaInstalacao().retornoInst))
     #dispatcher.add_handler(MessageHandler(Filters.text & Filters.command, medidor))
-    dispatcher.add_handler(MessageHandler(Filters.command, resultadoCont))
+    #dispatcher.add_handler(MessageHandler(Filters.command, resultadoCont))
 
     # Start the Bot
     updater.start_polling()
--- Teste_bot02/teste.py
+++ Teste_bot02/teste.py
@@ -2,6 +2,50 @@ import sqlite3
 
 from sqlite3 import Error
 
+
+########## Criar Conexão com BD e a tabela
+
+########## Realizar Consultas - Por Conta Contrato
+
+def ConexaoBanco():
+    caminho = "C:\\Projetos_JEB\\BD_Leitura\\BD_Leitura.db" # caminho do banco - "C:\\Projetos_JEB\\BD_Leitura\\BD_Leitura.db"
+    con = None
+
+    try:
+        con=sqlite3.connect(caminho)
+    except Error as ex:
+        print(ex)
+    return con
+
+vcon=ConexaoBanco()   
+
+def consultaContrato(conexao,sql):
+    c=conexao.cursor()
+    c.execute(sql)
+    resultado=c.fetchall()
+    return resultado
+
+entradaCont = 2089890015
+
+def resultadoCont():
+    vsql= f"SELECT * FROM BD_Leitura WHERE contrato = {entradaCont}" #interpolação para entrada do usuario
+    res=consultaContrato(vcon,vsql)   
+    if res:
+        for r in [res]:
+            for retorno  in r:
+                resultado = {retorno}
+                print(resultado)
+    else:
+        prints (f"\nNúmero invalido ou não localizado!\n")
+    
+
+def saidaDados():
+    print(resultadoCont())
+
+saidaDados()
+
+
+
 ###########################################################################################
 ###########################################################################################
 ################################## BANCO DE DADOS #########################################
@@ -44,7 +88,7 @@ resultadoCont()
 
 vcon.close()
 
-'''
+
 instalacao = ('Instalação: ')
                 medidor = ('Medidor: ')
                 contrato = ('Contrato: ')
@@ -63,7 +107,6 @@ instalacao = ('Instalação: ')
                 comentario = ('Comentario: ')
                 leiturista = ('Leiturista: ')
 
-'''
 
 
 
@@ -133,3 +176,268 @@ else:
     f"\nNúmero invalido ou não localizado!\n" 
 
 vcon.close()
+
+
+##################################################################################################
+
+
+import requests
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          RegexHandler, ConversationHandler, CallbackQueryHandler)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+
+
+STATE1 = 1
+STATE2 = 2
+
+def welcome(update, context):
+    try:
+        username = update.message.from_user.username
+        firstName = update.message.from_user.first_name
+        lastName = update.message.from_user.last_name
+        message = "Olá, " + firstName + " " + lastName + ", Let's Start!"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    except Exception as e:
+        print(str(e))
+
+
+def feedback(update, context):
+    try:
+        message = 'Por favor, digite um feedback para ajudar com o nosso crescimento:'
+        update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
+        return STATE1
+    except Exception as e:
+        print(str(e))
+
+
+def inputFeedback(update, context):
+    feedback = update.message.text
+    print(feedback)
+    if len(feedback) > 10:
+        message = """Seu feedback foi muito curtinho... 
+                        \nInforma mais pra gente, por favor?"""
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE1
+    else:
+        message = "Muito obrigado pelo seu feedback!"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return ConversationHandler.END
+
+
+def inputFeedback2(update, context):
+    feedback = update.message.text
+    message = "Muito obrigado pelo seu feedback!"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    return ConversationHandler.END
+
+
+# https://getemoji.com/
+def askForNota(update, context):
+    try:
+        question = 'Qual nota você dá para o tutorial?'
+        keyboard = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("👎 1", callback_data='1'),
+              InlineKeyboardButton("2", callback_data='2'),
+              InlineKeyboardButton("🤔 3", callback_data='3'),
+              InlineKeyboardButton("4", callback_data='4'),
+              InlineKeyboardButton("👍 5", callback_data='5')]])
+        update.message.reply_text(question, reply_markup=keyboard)
+    except Exception as e:
+        print(str(e))
+
+
+def getNota(update, context):
+    try:
+        query = update.callback_query
+        print(str(query.data))
+        message = 'Obrigado pela sua nota: ' + str(query.data) 
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    except Exception as e:
+        print(str(e))
+
+
+def cancel(update, context):
+    return ConversationHandler.END
+
+# aqui
+
+STATE3 = 3
+STATE4 = 4
+
+def instalacao(update, context):
+    try:
+        username = update.message.from_user.username
+        firstName = update.message.from_user.first_name
+        lastName = update.message.from_user.last_name
+        message = "Por favor, "+ firstName + " " + lastName + " entre com o número da instalação!"
+        update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
+        return STATE3
+    except Exception as e:
+        print(str(e))
+
+
+def inputInstalacao(update, context):
+    instalacao = update.message.text
+    print(instalacao)
+    if len(instalacao) < 7:
+        message = """Número da instalação está incorreta... 
+                        \nPor gentileza informe o número correto!"""
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE3
+    else:
+        message = "Segue conforme solicitado: " +  "Ajudo em algo mais?"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return ConversationHandler.END
+
+def inputInstalacao2(update, context):
+    instalacao = update.message.text
+    message = "Segue conforme solicitado: " + "Ajudo em algo mais?"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    return ConversationHandler.END
+
+def cancel(update, context):
+    return ConversationHandler.END
+
+
+STATE5 = 5
+STATE6 = 6
+
+
+def contacontrato(update, context):
+    try:
+        username = update.message.from_user.username
+        firstName = update.message.from_user.first_name
+        lastName = update.message.from_user.last_name
+        message = "Por favor, "+ firstName + " " + lastName + " entre com o número da conta contrato!"
+        update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
+        return STATE5
+    except Exception as e:
+        print(str(e))
+
+
+def inputContacontrato(update, context):
+    contacontrato = update.message.text
+    print(contacontrato)
+    if len(contacontrato) < 10:
+        message = """Número da conta contrato está incorreto... 
+                        \nPor gentileza informe o número correto!"""
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE5
+    else:
+        message = "Segue conforme solicitado: " +  "Ajudo em algo mais?"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return ConversationHandler.END
+
+def inputContacontrato2(update, context):
+    contacontrato = update.message.text
+    message = "Segue conforme solicitado: " + "Ajudo em algo mais?"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    return ConversationHandler.END
+
+def cancel(update, context):
+    return ConversationHandler.END
+
+STATE7 = 7
+STATE8 = 8
+
+
+def medicao(update, context):
+    try:
+        username = update.message.from_user.username
+        firstName = update.message.from_user.first_name
+        lastName = update.message.from_user.last_name
+        message = "Por favor, "+ firstName + " " + lastName + " entre com o número do equipamento de medição!"
+        update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
+        return STATE7
+    except Exception as e:
+        print(str(e))
+
+
+def inputMedicao(update, context):
+    medicao = update.message.text
+    print(medicao)
+    if len(medicao) < 8:
+        message = """O número da medição está incorreto... 
+                        \nPor gentileza informe o número correto!"""
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE7
+    else:
+        message = "Segue conforme solicitado: " +  "Ajudo em algo mais?"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return ConversationHandler.END
+
+def inputMedicao2(update, context):
+    medicao = update.message.text
+    message = "Segue conforme solicitado: " + "Ajudo em algo mais?"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    return ConversationHandler.END
+
+def cancel(update, context):
+    return ConversationHandler.END
+
+# aqui
+
+
+
+def main():
+    try:
+        # token = os.getenv('TELEGRAM_BOT_TOKEN', None)
+        token = '1566848654:AAEBS9TcwAHm5wh93Z5BtQoi9k5UB44doYg'
+        updater = Updater(token=token, use_context=True)
+
+        updater.dispatcher.add_handler(CommandHandler('start', welcome))
+
+        conversation_handler = ConversationHandler(
+            entry_points=[CommandHandler('feedback', feedback)],
+            states={
+                STATE1: [MessageHandler(Filters.text, inputFeedback)],
+                STATE2: [MessageHandler(Filters.text, inputFeedback2)]
+            },
+            fallbacks=[CommandHandler('cancel', cancel)])
+        updater.dispatcher.add_handler(conversation_handler)
+
+        # aqui
+
+        conversation_handler = ConversationHandler(
+            entry_points=[CommandHandler('instalacao', instalacao)],
+            states={
+                STATE3: [MessageHandler(Filters.text, inputInstalacao)],
+                STATE4: [MessageHandler(Filters.text, inputInstalacao2)]
+            },
+            fallbacks=[CommandHandler('cancel', cancel)])
+        updater.dispatcher.add_handler(conversation_handler)
+
+        conversation_handler = ConversationHandler(
+            entry_points=[CommandHandler('contacontrato', contacontrato)],
+            states={
+                STATE5: [MessageHandler(Filters.text, inputContacontrato)],
+                STATE6: [MessageHandler(Filters.text, inputContacontrato2)]
+            },
+            fallbacks=[CommandHandler('cancel', cancel)])
+        updater.dispatcher.add_handler(conversation_handler)
+
+        conversation_handler = ConversationHandler(
+            entry_points=[CommandHandler('medicao', medicao)],
+            states={
+                STATE7: [MessageHandler(Filters.text, inputMedicao)],
+                STATE8: [MessageHandler(Filters.text, inputMedicao2)]
+            },
+            fallbacks=[CommandHandler('cancel', cancel)])
+        updater.dispatcher.add_handler(conversation_handler)
+
+        # aqui
+
+        updater.dispatcher.add_handler(CommandHandler('nota', askForNota))
+        updater.dispatcher.add_handler(CallbackQueryHandler(getNota))
+
+        print("Updater no ar: " + str(updater))
+        updater.start_polling()
+        updater.idle()
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    main()
+
+'''
